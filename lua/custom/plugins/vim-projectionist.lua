f0651ae337184b86ef444e1970d66c3e039bfa2c
tafryn <tafryn@gmail.com>
2024-06-28 07:41:00 -0700

    Add alternate split mapping

--- lua/custom/plugins/vim-projectionist.lua
+++ lua/custom/plugins/vim-projectionist.lua
@@ -1,6 +1,9 @@
 return {
   { -- Configure per-project alternate files
     'tpope/vim-projectionist',
-    config = function() vim.api.nvim_set_keymap('n', 'ga', '<cmd>A<CR>', { desc = 'Alternate file' }) end,
+    config = function()
+      vim.api.nvim_set_keymap('n', 'ga', '<cmd>A<CR>', { desc = 'Alternate file' })
+      vim.api.nvim_set_keymap('n', 'go', '<cmd>AV<CR>', { desc = 'VSplit Alternate file' })
+    end,
   },
 }
